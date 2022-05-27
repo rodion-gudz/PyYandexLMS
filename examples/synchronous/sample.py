f93ac3249b1755859f027e37d82904b78bd6e48f
lav <me@lavn.ml>
2022-05-27 16:24:27 +0300

    example update

--- examples/synchronous/sample.py
+++ examples/synchronous/sample.py
@@ -8,7 +8,10 @@ password = getenv("PASSWORD")
 
 def main():
     client = Client(login, password)
-    print(client.check_authorized())
+    user = client.get_user()
+    print(f"Имя: {user.profile.display_name}")
+    print(f"Дата регистрации в LMS: {user.profile.date_joined.strftime('%d.%m.%Y')}")
+    print(f"Курсы: {', '.join(course.title for course in user.courses_summary.student)}")
 
 
 if __name__ == "__main__":
