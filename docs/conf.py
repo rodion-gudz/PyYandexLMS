5a7b2c17a9cc992bf2f33578129062bf98e00d2a
lav <me@lavn.ml>
2022-05-27 17:06:19 +0300

    add docs by `sphinx`

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+project = 'PyYandexLMS'
+copyright = '2022, lav.'
+author = 'lav.'
+
+release = '0.1.0'
+
+extensions = [
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.intersphinx",
+    "sphinx-prompt",
+    "sphinx_substitution_extensions",
+    "sphinx_copybutton",
+]
+
+templates_path = ["_templates"]
+html_theme = "furo"
+html_static_path = ["_static"]
+todo_include_todos = True
+pygments_style = "sphinx"
+htmlhelp_basename = project
+html_theme_options = {}
+html_css_files = [
+    "stylesheets/extra.css",
+]
+highlight_language = 'python3'
+
+language = 'ru'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
