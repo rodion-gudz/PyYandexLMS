90ee9e22f27068977e9ddfbb5abfa21e1b6f5f67
lav <me@lavn.ml>
2022-05-29 18:20:22 +0300

    remove debug print

--- examples/synchronous/sample.py
+++ examples/synchronous/sample.py
@@ -14,7 +14,6 @@ def main():
     print(
         f"Курсы: {', '.join(course.title for course in user.courses_summary.student)}"
     )
-    courses = user.courses_summary.student
 
 
 if __name__ == "__main__":
